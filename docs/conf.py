433f28f81d754e3ef3f9e1cfa1be8f258107bb2d
Masao Takaku <tmasao@acm.org>
2023-08-15 22:59:03 +0900

    add config files.

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'ttl2html'
+copyright = '2023, Masao Takaku'
+author = 'Masao Takaku'
+
+# release = '0.1'
+# version = '0.1'
+
+# -- General configuration
+
+language = 'ja'
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
